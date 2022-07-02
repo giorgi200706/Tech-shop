885197b57f54e7acb1b0beadbf9a935fb7b2c6bf
giorgi200706 <giorgich31@gmail.com>
2022-07-03 01:06:25 +0400

    .

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+let slider = tns({
+    container : ".my-slider",
+    "slideBy" : 1,
+    "speed"   : 400,
+    "nav"     : false,
+    autoplay: true,
+    controlsContainer: "#controls",
+    prevButton : ".previous",
+    nextButton : ".next",
+    items : 1,
+    gutter : 20,
+})
+
+// const element = document.querySelector('#tns1-ow');
+// element.style.display = 'none';
+
